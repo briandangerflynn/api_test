4dd479d7663aab44841c1716cfe67bce97928465
briandangerflynn <bdflynny@gmail.com>
2018-04-09 11:55:46 -0400

    updating changes and pushing to github and heroku

--- js/main.js
+++ js/main.js
@@ -1,53 +1,177 @@
 $(document).ready(function() {
 
-  $(document).click(function() {
+
+  // GLOBAL VARIABLES
+  //
+  //
+
+  var currentGarment;
+  var currentAlterations = [];
+  var alterations = [];
+  var currentItem;
+  var items = [];
+  var counter = 0;
+
+  //
+  //
+  // END OF GLOBAL VARIABLES
+
+
+  // GARMENT SELECTION PAGE
+  //
+  //
+
+  $("#pants").click(function() {
+    currentGarment = "pants"
+    $("#garment-select").toggleClass('hidden');
+    $("#alteration-select").toggleClass('hidden');
+    $(".pants-alteration").toggleClass('hidden');
+  });
+
+  $("#shirt").click(function() {
+    currentGarment = "shirt"
+    $("#garment-select").toggleClass('hidden');
+    $("#alteration-select").toggleClass('hidden');
+    $(".shirt-alteration").toggleClass('hidden');
+  });
+
+  $("#skirt").click(function() {
+    currentGarment = "skirt"
+    $("#garment-select").toggleClass('hidden');
+    $("#alteration-select").toggleClass('hidden');
+    $(".skirt-alteration").toggleClass('hidden');
+  });
+
+  $("#dress").click(function() {
+    currentGarment = "dress"
+    $("#garment-select").toggleClass('hidden');
+    $("#alteration-select").toggleClass('hidden');
+    $(".dress-alteration").toggleClass('hidden');
+  });
+
+  //
+  //
+  // END OF GARMENT SELECTION PAGE
+
+
+
+
+  // ALTERATION SELECTION PAGE
+  //
+  //
+  $(".alteration-card").click(function() {
+    currentAlterations.push(($(this).find("p").html()))
+    $(this).css('background-color', 'lightgrey');
+  });
+
+
+  //
+  //
+  // END OF ALTERATION SELECTION PAGE
+
+
+
+
+  // ALTERATION NAVIGATION BUTTONS
+  //
+  //
+
+  $("#alteration-back-button").click(function() {
+    $("." + currentGarment + "-alteration").toggleClass('hidden');
+    $("#alteration-select").toggleClass('hidden');
+    $("#garment-select").toggleClass('hidden');
+    $(".alteration-card").css('background-color', 'white');
+    currentAlterations = [];
+  });
+
+
+  $("#add-alt-to-basket").click(function() {
+    if (currentAlterations.length < 1){
+
+    } else {
+    if(items.length === 0){
+      $("#basket").toggleClass('hidden');
+    }
+    currentItem = {id: counter, garment: currentGarment, alterations: currentAlterations}
+    items.push(currentItem)
+    $("#basket-items").append("<p>"+ currentItem.garment + "<br>" + currentItem.alterations + "<p>")
+    $("." + currentGarment + "-alteration").toggleClass('hidden');
+    $(".alteration-card").css('background-color', 'white');
+    $("#alteration-select").toggleClass('hidden');
+    $("#garment-select").toggleClass('hidden');
+    currentAlterations = [];
+    counter = counter + 1
+    }
+  });
+
+  //
+  //
+  // END OF ALTERATION NAV BUTTONS
+
+
+   $("#add-order-details-button").click(function() {
+    $("main").toggleClass('hidden');
+    $("#order-details").toggleClass('hidden');
+  })
+
+
+
+
+
+
+  // POST REQUEST TO AIR TAILOR API
+  //
+  //
+  const data = {
+    order: {
+      requester_notes: 'these are notes the customer or revolve can leave',
+      items: [
+        {
+          item_type_id: 7,
+          alterations: [{ alteration_id: 208 }, { alteration_id: 219 }],
+        },
+        {
+          item_type_id: 6,
+          alterations: [{ alteration_id: 36 }],
+        },
+      ],
+      customer: {
+        first_name: 'Bob',
+        last_name: 'Builder',
+        phone: '1234567890',
+        email: 'bob@builder.com',
+        street: '123 B St',
+        street_two: 'Apt 2R',
+        city: 'New York',
+        state_province: 'NY',
+        zip_code: '10031',
+      },
+    },
+  };
+
+
+
+  $("#submit").click(function() {
     /* Act on the event */
 
     $.ajax({
-      url:'https://portal.airtailor.com/api/v1/orders',
-      headers: {
-          'X_Api_Key':'O7iq7W0Kcg8MynMp3aaHzgtt',
-          'Content-Type':'application/json'
-      },
+      url: 'https://portal.airtailor.com/api/v1/orders',
       method: 'POST',
-      dataType: 'json',
-      data: {
-        "order": {
-          "requester_notes": "these are notes the customer or revolve can leave",
-          "items": [
-            {
-              "item_type_id": 7,
-              "alterations": [
-                { "alteration_id": 208 },
-                { "alteration_id": 219 }
-              ]
-            },
-            {
-              "item_type_id": 6,
-              "alterations": [
-                { "alteration_id": 36 }
-              ]
-            }
-          ],
-          "customer": {
-            "first_name": "Bob",
-            "last_name": "Builder",
-            "phone": "1234567890",
-            "email": "bob@builder.com",
-            "street": "123 B St",
-            "street_two": "Apt 2R",
-            "city": "New York",
-            "state_province": "NY",
-            "zip_code": "10031"
-          }
-        }
-
+      headers: {
+        'X-Api-Key': 'O7iq7W0Kcg8MynMp3aaHzgtt',
+        'Content-Type': 'application/json',
       },
-      success: function(data){
-        console.log('success: '+data);
-      }
+      dataType: 'json',
+      data: JSON.stringify(data),
+      success: function(res) {
+        console.log('success', res);
+      },
+      error: function(res) {
+        console.log('error', res);
+      },
     });
   });
+
 });
 
 
